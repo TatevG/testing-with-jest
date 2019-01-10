67df35ddb86ff878ec76aefbca2d5afedcc99a88
TatevG <tatevgalstian@gmail.com>
2019-01-10 13:44:34 +0400

    starting jest and enzyme testing

---
+++ jest-and-enzyme-testing/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+// import "./App.css";
+
+//     "babel-loader": "^7.1.4",     "jest": "^22.4.4",
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello world!</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
